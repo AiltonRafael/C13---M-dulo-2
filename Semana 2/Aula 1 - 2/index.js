19f4ccd9a6d2462ae406a398d21a0d656da8ad02
Ailton Rafael <ailton.junior@al.infnet.edu.br>
2022-04-22 21:27:19 -0300

    Detalhes aula 3

--- Semana 2/Aula 1 - 2/index.js
+++ Semana 2/Aula 1 - 2/index.js
@@ -18,26 +18,42 @@ let cavaleiros = [ // banco de dados fake
         id: 1,
         nome: 'Airos',
         signos: 'Leão',
+        tipo: 'Ouro',
+        iframe: 'width=560 height=315 src=https://www.youtube.com/embed/jdHh18Ppyzg title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen',
         img: 'https://static.wikia.nocookie.net/ssu/images/8/8b/Aiolia_Armadura_Divina_card.png'
     },
     {   
         id: 2,
         nome: 'Aldebaran',
         signos: 'Touro',
+        tipo: 'Ouro',
+        iframe: 'width=560 height=315 src=https://www.youtube.com/embed/jdHh18Ppyzg title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen',
         img: 'https://pm1.narvii.com/6403/ffe9e1b19c99f2c7da6471cf290c2e7695c15573_hq.jpg'
     },
     {
         id: 3,
         nome: 'Miros',
         signos: 'Escopião',
+        tipo: 'Ouro',
+        iframe: 'width=560 height=315 src=https://www.youtube.com/embed/jdHh18Ppyzg title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen',
         img: 'https://i.pinimg.com/736x/3b/53/f9/3b53f97abecb8d86735ea953d8fe635c.jpg'
     },
     {
         id: 4,
         nome: 'Shaka',
+        tipo: 'Ouro',
+        iframe: 'width=560 height=315 src=https://www.youtube.com/embed/jdHh18Ppyzg title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen',
         signos: 'Virgem',
         img: 'http://pm1.narvii.com/7321/a4df13e5e19dd0fb961ae5e38b3ec6de84a40916r1-640-800v2_uhq.jpg'
-    }
+    },
+    {
+        id: 5,
+        nome: 'Ikki',
+        tipo: 'Bronze',
+        signos: 'Fênix',
+        iframe: 'width=560 height=315 src=https://www.youtube.com/embed/JwcL1JU5Wdo title=YouTube video player frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen',
+        img: 'https://static.wikia.nocookie.net/ssu/images/0/0d/1150.jpg'
+    },
 ]
 
 app.get('/', (req, res) => { // get é um método HTTP/HTTPS que serve para trazer uma pagina
@@ -47,14 +63,18 @@ app.get('/', (req, res) => { // get é um método HTTP/HTTPS que serve para traz
 })
 
 app.get('/detalhes/:id', (req, res) => {
-    let cavaleiro = []
+    let cavaleiro
     cavaleiros.filter((element) => {
         if(element.id == req.params.id){
-            cavaleiro.push(element)
+            cavaleiro = element
         }
     })
     console.log(cavaleiro)
     res.render('detalhes.ejs', {
         cavaleiro
     })
+})
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro.ejs')
 })
